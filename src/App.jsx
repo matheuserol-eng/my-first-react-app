1c186c3045f6941c139e0797e9cac5938eb5ffed
Matheus <matheuserol@gmail.com>
2025-09-29 19:13:29 -0300

    Fazendo o formulário

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,42 @@ import { TextField } from "./components/TextField";
 const App = () => {
   const [count, setCount] = useState(0)
   const [name, setName] = useState("")
-    const CARDS = [
-      {
-        id: 0,
-        title: 'Card A',
-        description: 'Olá eu sou filho do card A'
-      },
-      {
-        id: 1,
-        title: 'Card B',
-        description: 'Olá eu sou filho do card B'
-      },
-      {
-        id: 2,
-        title: 'Card C',
-        description: 'Olá eu sou filho do card C'
-      }
-    ]
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const CARDS = [
+    {
+      id: 0,
+      title: 'Card A',
+      description: 'Olá eu sou filho do card A'
+    },
+    {
+      id: 1,
+      title: 'Card B',
+      description: 'Olá eu sou filho do card B'
+    },
+    {
+      id: 2,
+      title: 'Card C',
+      description: 'Olá eu sou filho do card C'
+    }
+  ]
 
   const inc = () => setCount(c => c + 1)
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    console.log({ name,email,password })
+  }
+
   return (
-    <main className=" bg-slate-50">
+    <main className="min-h-dvh grid place-items-center bg-slate-50">
       <h1 className="text-3x1 font-bold text-slate-800">
         Hello Tailwind + React!
       </h1>
       <div className="container mx-auto p-6 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        {CARDS.map(item =>(
+        {CARDS.map(item => (
           <Card key={item.id} title={item.title}>
             <p>{item.description}</p>
           </Card>
@@ -44,16 +54,41 @@ const App = () => {
       </a>
 
       <Hello name="Matheus" />
-      <button onClick={inc} className="bg-blue-500 text-neutral-50 px-4 py-2 rounded-2x1 cursor-pointer">
+      <button onClick={inc} className="bg-blue-500 text-neutral-50 px-4 py-2 rounded-2xl cursor-pointer">
         Count: {count}
       </button>
-      <form>
-      <TextField
-      id='name'
-      label="Nome completo"
-      value={name}
-      onChange={event => setName(event.target.value)}
-      />
+
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id='name'
+          name="fullName"
+          label="Nome completo"
+          placeholder="Digite seu nome!"
+          type="text"
+          value={name}
+          onChange={event => setName(event.target.value)}
+        />
+        <TextField
+          id='email'
+          name="email"
+          label="E-mail completo"
+          placeholder="Digite seu email!"
+          type="email"
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+        />
+        <TextField
+          id='password'
+          name="password"
+          label="Sua senha"
+          placeholder="Digite sua senha aqui!"
+          type="password"
+          value={password}
+          onChange={event => setPassword(event.target.value)}
+        />
+        <button type="submit" className="bg-blue-500 text-neutral-50 px-4 py-2 rounded-2xl cursor-pointer text-center w-full">
+          Enviar
+        </button>
       </form>
     </main>
   );
--- src/components/TextField/index.jsx
+++ src/components/TextField/index.jsx
@@ -1,8 +1,8 @@
 export const  TextField = ({id, label, ...props}) => {
     return (
-        <fieldset className="flex flex-col space-y-2">
-            <label htmlfor={id} className="font-medium">{label}</label>
-        <input id={id} className="py-2 px-4 w-full" {...props} />
+        <fieldset className="flex flex-col space-y-2 mb-2">
+            <label htmlFor={id} className="font-medium">{label}</label>
+        <input id={id} className="py-2 px-4 border rounded-2xl w-full" {...props} />
         </fieldset>
     )
 }
